bb2671bfa4eb537ee4c8766a52c8e58f1a7e92fa
Steven Buglione <spbug1999@gmail.com>
2021-07-06 16:58:48 -0400

    Added Module02Lesson04

---
+++ Module02Lesson04/ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your age: ");
+            string ageText = Console.ReadLine();
+
+            //ageText = ageText + 10;
+            //ageText += 10;
+
+            //Console.WriteLine($"You are going to be { ageText } years old in 10 years.");
+
+            //int age = int.Parse(ageText);
+
+            bool isValidAge = int.TryParse(ageText, out int age);
+
+            if (isValidAge == true)
+            {
+
+                age += 10;
+
+                Console.WriteLine($"You are going to be { age } years old in 10 years.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a numerical value");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
