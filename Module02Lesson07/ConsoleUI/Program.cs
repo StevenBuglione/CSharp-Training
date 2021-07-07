87ac6a1903cee9081a4ad3485864f51c2eaf6150
StevenBuglione <steven.buglione1@marist.edu>
2021-07-07 17:46:01 -0400

    Added Module02Lesson07

---
+++ Module02Lesson07/ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] firstName = new string[5];
+
+            //firstName[0] = "Tim";
+            //firstName[1] = "Sue";
+            //firstName[2] = "Bob";
+            //firstName[4] = "Carl";
+
+            //Console.WriteLine($"My array has { firstName[0] }, { firstName[1] }, { firstName[2] }, { firstName[4] }");
+
+
+            //string data = "Tim,Sue,Bob,Frank,Sandra,Molly";
+            //string[] firstNames = data.Split(',');
+
+            //Console.WriteLine($"The third first name is { firstNames[2] }");
+
+            //int[] ages = new int[3];
+            //ages[0] = 4;
+            //ages[1] = 5;
+            //ages[2] = 54;
+
+            string[] lastNames = new string[] { "Cory", "Smith", "Jones" };
+
+            lastNames[1] = "Malcome";
+
+
+            Console.ReadLine();
+        }
+    }
+}
