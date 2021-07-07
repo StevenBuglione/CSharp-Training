09fd3a1eb72ecf3cea67d48d5093a807cccdefe9
StevenBuglione <steven.buglione1@marist.edu>
2021-07-07 18:08:06 -0400

    Added Module02Lesson08

---
+++ Module02Lesson08/ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstName = new List<string>();
+
+            firstName.Add("Tim");
+            firstName.Add("Sue");
+            firstName.Add("Bob");
+
+            Console.WriteLine($"The second element is { firstName[1]}");
+
+            firstName.Remove("Sue");
+
+            Console.WriteLine($"The second element is { firstName[1]}");
+
+            firstName[1] = firstName[1].ToUpper();
+
+            Console.WriteLine($"The second element is { firstName[1]}");
+
+            Console.ReadLine();
+        }
+    }
+}
