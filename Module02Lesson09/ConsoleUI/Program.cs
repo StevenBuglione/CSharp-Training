65594a424ca9972aaa68b3236082bd2d9211b7bf
StevenBuglione <steven.buglione1@marist.edu>
2021-07-07 18:27:47 -0400

    Added Module02Lesson09

---
+++ Module02Lesson09/ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> importantYears = new Dictionary<int, string>();
+
+            importantYears[1492] = "Columbus discovered the new world.";
+            importantYears[1969] = "Man walks on the moon.";
+            importantYears[1999] = "Steve was born";
+
+            Console.WriteLine($"In the year 1999, this happened: { importantYears[1999]}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
