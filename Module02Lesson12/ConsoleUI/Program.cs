209d81a67b315b3933882f4df4ee651f351c4a59
Steven Buglione <stevenbuglione@Stevens-Mini.home>
2021-07-08 18:47:13 -0400

    Added Module02Lesson12

---
+++ Module02Lesson12/ConsoleUI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayAuthor();
+
+            string firstName = GetUserName("What is your first name: ");
+            string lastName = GetUserName("What is your last name: ");
+
+            WelcomUser(firstName, lastName);
+            
+            Console.ReadLine();
+        }
+
+        private static void WelcomUser(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello { firstName } { lastName }");
+        }
+
+        private static void SayAuthor()
+        {
+            Console.WriteLine("**********************");
+            Console.WriteLine("Writen by: Steven Buglione");
+            Console.WriteLine("for the Founddation in C# course.");
+            Console.WriteLine("**********************");
+        }
+
+        private static string GetUserName(string message)
+        {
+            Console.WriteLine(message);
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        
+    }
+}
